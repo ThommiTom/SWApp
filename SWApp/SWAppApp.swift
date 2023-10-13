a98490c66f5f6ab3f2384898bcc3d35ee1c3f9ec
Thomas Schatton <t.schatton@me.com>
2023-10-13 13:21:40 +0200

    first commit

---
+++ SWApp/SWAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWAppApp.swift
+//  SWApp
+//
+//  Created by Thomas Schatton on 13.10.23.
+//
+
+import SwiftUI
+
+@main
+struct SWAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
