5fdf5c314e9b38d54ce0d503eeeda3fa67d3e85e
Thomas Schatton <t.schatton@me.com>
2023-10-13 15:56:42 +0200

    adds first ViewModel for Films
    manages and handles first acquired data from film endpoint

--- SWApp/ViewModels/FilmHandler.swift
+++ SWApp/ViewModels/FilmHandler.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+
+class FilmHandler: ObservableObject {
+    @Published var films = [Film]()
+    @Published var isDownloading = false
+
+    var sortedFilms: [Film] {
+        return films.sorted { $0.episodeId > $1.episodeId }
+    }
+
+    var filmUrls: [URL?] = []
+    var dispatchGroup = DispatchGroup()
+
+    init() {
+        // assumption: swapi.dev provides data for all 9 episodes
+        // observation: unfortantly REST calls to swapi often fail
+        for episodeNo in 1...9 {
+            let urlAsString = "https://swapi.dev/api/films/\(String(episodeNo))/"
+            filmUrls.append(URL(string: urlAsString))
+        }
+
+        getStarWarsMovies()
+    }
+}
+
+extension FilmHandler {
+    private func getStarWarsMovies() {
+        isDownloading = true
+        for url in filmUrls {
+            getStarWarsMovie(url: url)
+        }
+
+        dispatchGroup.notify(queue: DispatchQueue.main) {
+            self.isDownloading = false
+        }
+    }
+
+    private func getStarWarsMovie(url: URL?) {
+        dispatchGroup.enter()
+        Task {
+            await NetworkManager.shared.networkCall(with: url) { (result: Result<Film, NetworkError>) in
+                switch result {
+                case .success(let film):
+                    DispatchQueue.main.async {
+                        self.films.append(film)
+                    }
+                case .failure(let error):
+                    print(error.rawValue)
+                }
+
+                self.dispatchGroup.leave()
+            }
+        }
+    }
+}
