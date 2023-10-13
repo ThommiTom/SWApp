f33469208540c07929ea8906978793603aa19044
Thomas Schatton <t.schatton@me.com>
2023-10-13 15:04:16 +0200

    displays successfully loaded movies
    uses NetworkManager and https://swapi.dev/api/films/ endpoint to fetch movie data

--- SWApp/ContentView.swift
+++ SWApp/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var films = [Film]()
+    @State private var isDownloading = false
+
+    private var sortedFilms: [Film] {
+        return films.sorted { $0.episodeId > $1.episodeId }
+    }
+
+    var filmUrls: [URL?] = []
+    var dispatchGroup = DispatchGroup()
+
+    init() {
+        for episodeNo in 1...9 {
+            var urlAsString = "https://swapi.dev/api/films/\(String(episodeNo))/"
+            filmUrls.append(URL(string: urlAsString))
+        }
+    }
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if isDownloading {
+                ProgressView()
+            } else {
+                ForEach(sortedFilms, id: \.self) {
+                    Text($0.title)
+                }
+            }
+        }
+        .onAppear {
+            getStarWarsMovies()
         }
         .padding()
+    }
+
+    private func getStarWarsMovies() {
+        isDownloading = true
+        for url in filmUrls {
+            getStarWarsMovie(url: url)
+        }
+
+        dispatchGroup.notify(queue: DispatchQueue.main) {
+            isDownloading = false
+        }
+    }
+
+    private func getStarWarsMovie(url: URL?) {
+        dispatchGroup.enter()
+        Task {
+            await NetworkManager.shared.networkCall(with: url) { (result: Result<Film, NetworkError>) in
+                switch result {
+                case .success(let film):
+                    DispatchQueue.main.async {
+                        self.films.append(film)
+                    }
+                case .failure(let error):
+                    print(error.rawValue)
+                }
+
+                dispatchGroup.leave()
+            }
+        }
     }
 }
 
